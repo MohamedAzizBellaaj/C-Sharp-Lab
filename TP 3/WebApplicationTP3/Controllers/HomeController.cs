3c167bec3a52ad664a60e3977a1d4359c48f9d98
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2022-12-02 18:55:58 +0100

    TP 3

--- TP 3/WebApplicationTP3/Controllers/HomeController.cs
+++ TP 3/WebApplicationTP3/Controllers/HomeController.cs
@@ -19,43 +19,39 @@ namespace WebApplicationTP3.Controllers
 		public IActionResult Index()
 		{
 			SQLiteConnection dbcon = new SQLiteConnection("Data Source=.\\2022 GL3 .NET Framework TP3 - SQLite database.db;");
-			//// *Select every person using sql command:
-			//using (dbcon)
-			//{
-			//	dbcon.Open();
-			//	SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM personal_info", dbcon);
-			//	SQLiteDataReader reader = cmd.ExecuteReader();
-			//	using (reader)
-			//	{
-			//		while (reader.Read())
-			//		{
-			//			int id = (int)reader["id"];
-			//			string first_name = (string)reader["first_name"];
-			//			string last_name = (string)reader["last_name"];
-			//			string email = (string)reader["email"];
-			//			string image = (string)reader["image"];
-			//			string country = (string)reader["country"];
-			//			//DateTime date_birth = (DateTime)reader["date_birth"];
-			//			Debug.WriteLine($"id : {id}, first_name : {first_name}, last_name : {last_name}, email : {email}, image : {image}, country : {country}");
-			//		}
-			//	}
-			//}
+			// *Select every person using sql command:
+			using (dbcon)
+			{
+				dbcon.Open();
+				SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM personal_info", dbcon);
+				SQLiteDataReader reader = cmd.ExecuteReader();
+				using (reader)
+				{
+					while (reader.Read())
+					{
+						int id = (int)reader["id"];
+						string first_name = (string)reader["first_name"];
+						string last_name = (string)reader["last_name"];
+						string email = (string)reader["email"];
+						string image = (string)reader["image"];
+						string country = (string)reader["country"];
+						//DateTime date_birth = (DateTime)reader["date_birth"];
+						Debug.WriteLine($"id : {id}, first_name : {first_name}, last_name : {last_name}, email : {email}, image : {image}, country : {country}");
+					}
+				}
+			}
 			//// *Select every person using GetAllPerson:
 			//foreach (Person p in Personal_info.GetAllPerson(dbcon))
 			//{
 			//	Console.WriteLine(p);
 			//}
-			// *Select person with corresponding id using GetPerson
-			Person? p = Personal_info.GetPerson(5, dbcon);
-			if (p is not null)
-			{
-				Console.WriteLine(p);
-			}
-			return View();
-		}
-		public IActionResult Person()
-		{
-			return View();
+			////*Select person with corresponding id using GetPerson
+			//Person? p = Personal_info.GetPerson(5, dbcon);
+			//if (p is not null)
+			//{
+			//	Console.WriteLine(p);
+			//}
+			//return View();
 		}
 		public IActionResult Privacy()
 		{
---
+++ TP 3/WebApplicationTP3/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SQLite;
+using WebApplicationTP3.Models;
+
+namespace WebApplicationTP3.Controllers
+{
+	public class PersonController : Controller
+	{
+		[Route("/Person")]
+		[Route("Person/All")]
+		public IActionResult All()
+		{
+			SQLiteConnection dbcon = new SQLiteConnection("Data Source=.\\2022 GL3 .NET Framework TP3 - SQLite database.db;");
+			ViewBag.Message = Personal_info.GetAllPerson(dbcon);
+			return View();
+		}
+		[Route("Person/{id?}")]
+		public IActionResult GetById(int id)
+		{
+			SQLiteConnection dbcon = new SQLiteConnection("Data Source=.\\2022 GL3 .NET Framework TP3 - SQLite database.db;");
+			ViewBag.Message = Personal_info.GetPerson(id, dbcon);
+			return View();
+		}
+		[Route("Person/Search")]
+		public IActionResult Search()
+		{
+			return View();
+		}
+	}
+}
