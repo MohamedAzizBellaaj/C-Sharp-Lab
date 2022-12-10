65941baa005d61925e9d36f03baec1e684aaab23
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2022-12-10 19:22:43 +0100

    TP4

---
+++ TP 4/WebApplicationTP 4/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationTP_4.Data;
+using WebApplicationTP_4.Models;
+
+namespace WebApplicationTP_4.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			UniversityContext u = UniversityContext.getInstance();
+			List<Student> s = u.Student.ToList<Student>();
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			UniversityContext u = UniversityContext.getInstance();
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ TP 4/WebApplicationTP 4/Data/UniversityContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebApplicationTP_4.Models;
+
+namespace WebApplicationTP_4.Data
+{
+	public class UniversityContext : DbContext
+	{
+		public DbSet<Student> Student { get; set; }
+		private static UniversityContext Instance;
+
+		private UniversityContext(DbContextOptions o) : base(o) { }
+		private static UniversityContext Instantiate_UniversityContext()
+		{
+			Console.WriteLine("This is run.");
+			var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+			optionsBuilder.UseSqlite("Data Source=.\\2022 GL3 .NET Framework TP4 - SQLite database.db;");
+			return new UniversityContext(optionsBuilder.Options);
+		}
+		public static UniversityContext getInstance()
+		{
+			if (Instance is null)
+			{
+				Instance = Instantiate_UniversityContext();
+			}
+			return Instance;
+		}
+	}
+}
